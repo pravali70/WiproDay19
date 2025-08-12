68ba20d05947fb7ad0de611024e3a43812bf47ee
Pravali Parvataneni <pravaliparvataneni@gmail.com>
2025-08-12 12:15:38 +0530

    Added hello-app src folder

---
+++ hello-app/src/components/first/first.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+const First = () => {
+  return(
+    <div>
+      <p>This is My First Component in React...</p>
+    </div>
+  )
+}
+
+export default First;
